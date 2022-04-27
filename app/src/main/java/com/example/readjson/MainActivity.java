dcbcf149361831f3598b7683564b31900bef14f9
e21krida <e21krida@student.his.se>
2022-04-27 14:32:00 +0200

    added gson library

--- app/src/main/java/com/example/readjson/MainActivity.java
+++ app/src/main/java/com/example/readjson/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.readjson;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "==>";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String json = readFile("mountains.json");
+        Log.d(TAG, json);
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private String readFile(String fileName) {
+        try {
+            //noinspection CharsetObjectCanBeUsed
+            return new Scanner(getApplicationContext().getAssets().open(fileName), Charset.forName("UTF-8").name()).useDelimiter("\\A").next();
+        } catch (IOException e) {
+            Log.e(TAG, "Could not read file: " + fileName);
+            return null;
+        }
     }
 }
